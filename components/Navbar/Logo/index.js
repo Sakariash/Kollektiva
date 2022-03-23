6e3e77719c5e2703a6b71bc008d7e091f3fa8a66
Sakariash <simonsakarias@gmail.com>
2022-03-23 14:07:16 +0100

    tailwind added

---
+++ components/Navbar/Logo/index.js
@@ -0,0 +1,7 @@
+import myLogo from "../../../assets/images/headphones.svg";
+
+const Logo = () => {
+  return <img src={myLogo}></img>;
+};
+
+export default Logo;
---
+++ components/Navbar/index.js
@@ -0,0 +1,10 @@
+import Logo from "./Logo";
+const Navbar = () => {
+  return (
+    <div className="w-screen bg-orange-300">
+      <Logo />
+    </div>
+  );
+};
+
+export default Navbar;
