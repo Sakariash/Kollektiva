33d2c00e36c03f1cf6a2c143936947b6c7ce843b
LinneaEriksson <Linnea-E@hotmail.com>
2022-03-31 15:52:08 +0200

    fixed final loginstyling

--- components/Login/LoginForm/index.js
+++ components/Login/LoginForm/index.js
@@ -1,22 +1,22 @@
 const LoginForm = (props) => {
   return (
-    <div className="flex flex-col items-center content-center mt-20">
+    <div className="flex flex-col items-center content-center mt-20 tablet:mt-10">
       <div className="w-3/4 tablet:h-12 tablet:w-1/3">
         <label className="block" for="email">
           <p className="font-bold">Emailadress</p>
         </label>
         <input
-          className="bg-secondary h-10 w-full rounded-md tablet:h-12 tablet:w-full"
+          className="bg-secondary h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
           type="text"
           placeholder="Namn@exempel.se"
           name="email"
           required
         ></input>
         <label for="password" className="block">
-          <p className="mt-4 font-bold">Lösenord</p>
+          <p className="mt-6 font-bold">Lösenord</p>
         </label>
         <input
-          className="bg-secondary h-10 w-full rounded-md tablet:h-12 tablet:w-full"
+          className="bg-secondary h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
           type="password"
           placeholder="************"
           name="password"
@@ -24,7 +24,7 @@ const LoginForm = (props) => {
         ></input>
         <div className="flex justify-center tablet:justify-start">
           <button
-            className="bg-btnPrimary mt-20 h-14 w-3/4 font-bold rounded-md tablet:h-16 w-3/6"
+            className="bg-btnPrimary mt-20 h-14 w-3/4 font-bold rounded-md tablet:h-16 tablet:w-3/6 tablet:mt-6"
             type="submit"
           >
             Logga in
---
+++ components/Login/LoginText/index.js
@@ -0,0 +1,12 @@
+const LoginText = () => {
+  return (
+    <div className="hidden tablet:flex justify-center content-center mt-10">
+      <p className="font-bold flex justify-center w-1/3">
+        För att använda våra tjänster så måste du vara inloggad på ditt konto.
+        Har du inget konto? Bli medlem här!
+      </p>
+    </div>
+  );
+};
+
+export default LoginText;
