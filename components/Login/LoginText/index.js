ba8f81b7f9ab426c32e83e01307174720fc9d8f0
Sakariash <simonsakarias@gmail.com>
2022-04-05 22:07:55 +0200

    tailwind color added

--- components/Login/LoginText/index.js
+++ components/Login/LoginText/index.js
@@ -1,9 +1,11 @@
-const LoginText = () => {
+import Link from "next/Link";
+
+const LoginText = ({ SignupLink }) => {
   return (
     <div className="hidden tablet:flex justify-center content-center mt-10">
-      <p className="font-bold flex justify-center tablet:w-3/5 laptop:w-1/3">
+      <p className="font-bold justify-center tablet:w-3/5 laptop:w-1/3">
         För att använda våra tjänster så måste du vara inloggad på ditt konto.
-        Har du inget konto? Bli medlem här!
+        Har du inget konto? {SignupLink}
       </p>
     </div>
   );
