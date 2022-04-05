11caffcdda4aa312734ab44bec78653ed33ab89d
Sakariash <simonsakarias@gmail.com>
2022-04-05 15:56:22 +0200

    Page structure setup

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
-import Login from "../components/Login";
 import Footer from "../components/Footer";
-import BecomeMember from "../components/BecomeMember";
 
 export default function Home() {
-  return [<Navbar />, <BecomeMember />, <Footer />];
+  return [<Navbar />, <Footer />];
 }
---
+++ pages/signup/index.js
@@ -0,0 +1,7 @@
+import BecomeMember from "../../components/BecomeMember";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+export default function SignUpPage() {
+  return [<Navbar />, <BecomeMember />, <Footer />];
+}
