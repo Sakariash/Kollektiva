222bd137a502f1add4be3e21d20206f52c61384c
Sakariash <simonsakarias@gmail.com>
2022-04-05 22:44:35 +0200

    nav links updated

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,12 +1,20 @@
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import Hamburger from "./Hamburger";
+import Link from "next/link";
+Link;
 
 const Navbar = () => {
   return (
     <div className="px-5 w-screen h-20 flex justify-between items-center bg-secondary tablet:px-11 tablet:bg-white">
       <Logo />
-      <NavLinks />
+      <NavLinks
+        NavLinks={[
+          <Link href="/">Hyr ut ditt boende</Link>,
+          <Link href="/">Hyr ett boende</Link>,
+          <Link href="/">Mina sidor</Link>,
+        ]}
+      />
       <Hamburger />
     </div>
   );
