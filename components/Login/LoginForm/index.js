5617d3e3ea51f3faed668837c94352821d281d4c
LinneaEriksson <Linnea-E@hotmail.com>
2022-04-17 21:08:19 +0200

    fixed media queries

--- components/Login/LoginForm/index.js
+++ components/Login/LoginForm/index.js
@@ -1,12 +1,12 @@
 const LoginForm = (props) => {
   return (
-    <div className="flex flex-col items-center content-center mt-20 tablet:mt-10">
+    <div className="flex flex-col items-center content-center mt-10">
       <div className="w-3/4 tablet:w-3/5 laptop:w-1/3">
         <label className="block" for="email">
           <p className="font-bold">E-postadress</p>
         </label>
         <input
-          className="bg-secondary h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
+          className="bg-white shadow-lg shadow-slate-300 h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
           type="text"
           placeholder="Namn@exempel.se"
           name="email"
@@ -16,7 +16,7 @@ const LoginForm = (props) => {
           <p className="mt-6 font-bold">Lösenord</p>
         </label>
         <input
-          className="bg-secondary h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
+          className="bg-white shadow-lg shadow-slate-300 h-10 w-full rounded-md pl-2 tablet:h-12 tablet:w-full"
           type="password"
           placeholder="************"
           name="password"
