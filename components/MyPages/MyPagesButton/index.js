c2beff00c186a846b98068936a2cb0d798d5704f
LinneaEriksson <Linnea-E@hotmail.com>
2022-04-17 23:02:15 +0200

    added more mediaqueries

--- components/MyPages/MyPagesButton/index.js
+++ components/MyPages/MyPagesButton/index.js
@@ -1,6 +1,9 @@
 export default function MyPagesButton({ text, link }) {
   return (
-    <button className="bg-btnPrimary p-2 m-2 w-96 rounded-md" href={link}>
+    <button
+      className="bg-btnPrimary p-2 m-2 w-64 tablet:w-96 rounded-md"
+      href={link}
+    >
       <a href={link}> {text}</a>
     </button>
   );
