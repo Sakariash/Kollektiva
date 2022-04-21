6450b588589421e5d958bc7a7836086f72971fa4
Sakariash <simonsakarias@gmail.com>
2022-04-21 10:30:06 +0200

    Information box component added

---
+++ components/FronPageInfoBox/index.js
@@ -0,0 +1,14 @@
+const InfoBox = ({ imgsrc, boxText }) => {
+  return (
+    <div className="flex w-full justify-center tablet:justify-around tablet:mx-2">
+      <div className="bg-white shadow-lg drop-shadow-md mt-10 tablet:mt-14 mb-1 rounded-lg flex flex-col max-w-sm tablet:max-w-ms">
+        <img className="w-14 pt-7 mr-auto ml-auto" src={imgsrc}></img>
+        <p className="p-8 font-Inter tablet:font-medium desktop:text-xl text-lg text-transparent leading-8 font-normal mr-auto ml-auto text-center">
+          {boxText}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBox;
--- components/FrontPageInfo/index.js
+++ components/FrontPageInfo/index.js
@@ -1,22 +1,23 @@
-import RegisteredHousesBox from "../RegisterdHouses/RegisteredHousesBox";
+import InfoBox from "../FronPageInfoBox";
+
 export default function FrontPageInfo() {
   return (
     <>
       <div className="bg-primary px-2.5 flex flex-col h-full justify-items-center items-center justify-around py-6 tablet:flex-row desktop:relative desktop:h-96">
         <div className="desktop:absolute tablet:flex tablet:justify-between tablet:flex-row top-[-100px]">
-          <RegisteredHousesBox
+          <InfoBox
             imgsrc="assets/images/RegisteredHouses/Vector.png"
             boxText={
               "Oroa dig inte! Vi garanterar dig att det är 100% säkert att använda Kollektivas tjänster. Även om du söker ny bostad eller vill hyra ut."
             }
           />
-          <RegisteredHousesBox
+          <InfoBox
             imgsrc="assets/images/RegisteredHouses/Vector2.png"
             boxText={
               "Vi tar hand om krångelt! Kollektiva ansvarar för X, Y och Z. Vi vill att du som söker eller hyr ut din bostad ska kunna göra så smärttfritt som möjligt."
             }
           />
-          <RegisteredHousesBox
+          <InfoBox
             imgsrc="assets/images/RegisteredHouses/Vector3.png"
             boxText={
               "Vi tar 10% i provision för att agera mellanhand. Du som hyresvärd kan fortsätta amortera som vanligt när du hyr ut."
